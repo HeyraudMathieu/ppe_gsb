4a7aea3be544fd5d037b7ab44ac687b23ab9b830
Mathieu <matheyraud@gmail.com>
2021-01-26 20:52:38 +0100

    mise en place connexion bdd : user et mdp

--- includes/class.pdogsb.inc.php
+++ includes/class.pdogsb.inc.php
@@ -41,7 +41,7 @@ class PdoGsb
     private static $serveur = 'mysql:host=localhost';
     private static $bdd = 'dbname=gsb_frais';
     private static $user = 'userGsb';
-    private static $mdp = 'secret';
+    private static $mdp = 'ppegsblmr2021*';
     private static $monPdo;
     private static $monPdoGsb = null;
 
