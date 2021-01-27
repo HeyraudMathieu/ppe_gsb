db794dbe480157832869efa573799b13e42bdfc2
Mathieu <matheyraud@gmail.com>
2021-01-27 22:30:33 +0100

    Task #11 - Modif annexe : Ajout de l'autocomplete add :  - '/includes/autocomplete.php' update :  - '/includes/js/fonctions.js'

--- includes/js/fonctions.js
+++ includes/js/fonctions.js
@@ -14,9 +14,42 @@
  */
 
 /**
- * Fonction qui permet de gérer l'autocomplète sur un input
+ * Fonction qui permet l'autcomplete sur un input
  * 
  * @param {type} idInput
  * @param {type} nomFonctionPdo
+ * @param {type} valeur
+ * @param {type} label
  * @returns {undefined}
  */
+function autocomplete(idInput, nomFonctionPdo, valeur, label){
+        $("#" + idInput).autocomplete({
+            source : function( request, response ) {
+                $.ajax({
+                    type: "POST",
+                    url: "/includes/autocomplete.php",
+                    dataType: "json",
+                    data: {
+                        input: request.term,
+                        nomFonctionPdo: nomFonctionPdo,
+                        valeur: valeur,
+                        label: label
+                    },
+                    success: function(data){
+                        response(data);
+                    },
+                    error: function (xhr, thrownError) {
+                        alert(xhr.statusText);
+                        alert(xhr.responseText);
+                        alert(xhr.status);
+                        alert(thrownError);
+                    }
+                });
+            },
+            select: function (event, ui) {
+                // Action effectuée lors de la sélection d'un élément de l'autocomplete
+                // fonction à écrire sur la vue utilisant la fonction autocomplete
+                autocomplete_select(event, ui);
+            }
+        });
+    }
