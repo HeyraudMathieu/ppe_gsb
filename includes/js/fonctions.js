4b046d9ba926926e5309f6f437631745bba7c407
Mathieu <matheyraud@gmail.com>
2021-01-27 20:38:35 +0100

    Task #12 - Modif annexe : JQuery Ajout de JQuery, JQuery ui Ajout fichier '/includes/js/fonctions.js'

---
+++ includes/js/fonctions.js
@@ -0,0 +1,22 @@
+/**
+ * Fonctions javascript globales pouvant être utilisées par les vues de l'application
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    Mahieu HEYRAUD <matheyraud@gmail.com>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+
+/**
+ * Fonction qui permet de gérer l'autocomplète sur un input
+ * 
+ * @param {type} idInput
+ * @param {type} nomFonctionPdo
+ * @returns {undefined}
+ */
--- vues/shared/v_entete.php
+++ vues/shared/v_entete.php
@@ -25,6 +25,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="./styles/bootstrap/bootstrap.css" rel="stylesheet">
         <link href="./styles/style.css" rel="stylesheet">
+        
+        <!-- Jquery, Jquery ui, fichier javascript -->
+        <script src="/includes/js/fonctions.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        
     </head>
     <body>
         <div class="container">
