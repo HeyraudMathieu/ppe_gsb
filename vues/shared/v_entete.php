11e7b87aa66192f79757c8142e8d33c8ccfb0ae1
Léo CORMORECHE <leocormo@gmail.com>
2021-02-26 08:46:24 +0100

    Fix erreur pull bis

--- vues/shared/v_entete.php
+++ vues/shared/v_entete.php
@@ -49,58 +49,76 @@
             if ($estConnecte) {
                 ?>
                 <div class="header">
-                <div class="row vertical-align">
-                    <div class="col-md-4">
-                        <h1>
-                            <img src="./images/logo.jpg" class="img-responsive" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin">
-                        </h1>
-                    </div>
-                    <div class="col-md-8">
-                        <ul class="nav nav-pills pull-right" role="tablist">
-                            <li <?php if (!$uc || $uc == 'accueil') { ?>class="active" <?php } ?>>
-                                <a href="index.php">
-                                    <span class="glyphicon glyphicon-home"></span>
-                                    Accueil
-                                </a>
-                            </li>
-                            <?php if ($_SESSION['droit'] == 1) { ?>
-                                <li <?php if ($uc == 'gererFrais') { ?>class="active" <?php } ?>>
-                                    <a href="index.php?uc=gererFrais&action=saisirFrais">
-                                        <span class="glyphicon glyphicon-pencil"></span>
-                                        Renseigner la fiche de frais
-                                    </a>
-                                </li>
-                                <li <?php if ($uc == 'etatFrais') { ?>class="active" <?php } ?>>
-                                    <a href="index.php?uc=etatFrais&action=selectionnerMois">
-                                        <span class="glyphicon glyphicon-list-alt"></span>
-                                        Afficher mes fiches de frais
-                                    </a>
-                                </li>
-                            <?php } else { ?>
-                                <li <?php if ($uc == 'validerFrais') { ?>class="active" <?php } ?>>
-                                    <a href="index.php?uc=validerFrais&action=selectionnerVisiteurMois">
-                                        <span class="glyphicon glyphicon-pencil"></span>
-                                        Valider les fiches de frais
-                                    </a>
-                                </li>
-                                <li <?php if ($uc == 'suivrePaiement') { ?>class="active" <?php } ?>>
-                                    <a href="index.php?uc=suivrePaiement&action=selectionnerVisiteurMois">
-                                        <span class="glyphicon glyphicon-euro"></span>
-                                        Suivre le paiement des fiches de frais
+                    <div class="row vertical-align">
+                        <div class="col-md-4">
+                            <h1>
+                                <img src="./images/logo.jpg" class="img-responsive" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin">
+                            </h1>
+                        </div>
+                        <div class="col-md-8">
+                            <ul class="nav nav-pills pull-right" role="tablist">
                                 <li <?php if (!$uc || $uc == 'accueil') { ?>class="active" <?php } ?>>
+                                    <a href="index.php">
+                                        <span class="glyphicon glyphicon-home"></span>
+                                        Accueil
                                     </a>
                                 </li>
-                            <?php } ?>
-                            <li <?php if ($uc == 'deconnexion') { ?>class="active" <?php } ?>>
-                                <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
-                                    <span class="glyphicon glyphicon-log-out"></span>
-                                    Déconnexion
-                                </a>
-                            </li>
-                        </ul>
+                                <?php if ($_SESSION['droit'] == 1) { ?>
+                                    <li <?php if ($uc == 'gererFrais') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=gererFrais&action=saisirFrais">
+                                            <span class="glyphicon glyphicon-pencil"></span>
+                                            Renseigner la fiche de frais
+                                        </a>
+                                    </li>
+                                    <li <?php if ($uc == 'etatFrais') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=etatFrais&action=selectionnerMois">
+                                            <span class="glyphicon glyphicon-list-alt"></span>
+                                            Afficher mes fiches de frais
+                                        </a>
+                                    </li>
+                                    <li <?php if ($uc == 'deconnexion') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
+                                            <span class="glyphicon glyphicon-log-out"></span>
+                                            Déconnexion
+                                        </a>
+                                    </li>
+                                <?php } elseif ($_SESSION['droit'] == 2) { ?>
+                                    <li <?php if ($uc == 'validerFrais') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=validerFrais&action=selectionnerVisiteurMois">
+                                            <span class="glyphicon glyphicon-pencil"></span>
+                                            Valider les fiches de frais
+                                        </a>
+                                    </li>
+                                    <li <?php if ($uc == 'suivrePaiement') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=suivrePaiement&action=selectionnerVisiteurMois">
+                                            <span class="glyphicon glyphicon-euro"></span>
+                                            Suivre le paiement des fiches de frais
+                                        </a>
+                                    </li>
+                                    <li <?php if ($uc == 'deconnexion') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
+                                            <span class="glyphicon glyphicon-log-out"></span>
+                                            Déconnexion
+                                        </a>
+                                    </li>
+                                <?php } else { ?>
+                                    <li <?php if ($uc == 'gestion') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=gestion&action=getLesVisiteurs">
+                                            <span class="glyphicon glyphicon-user"></span>
+                                            Gestion
+                                        </a>
+                                    </li>
+                                    <li <?php if ($uc == 'deconnexion') { ?>class="active" <?php } ?>>
+                                        <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
+                                            <span class="glyphicon glyphicon-log-out"></span>
+                                            Déconnexion
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
                 <?php
             } else {
                 ?>   
